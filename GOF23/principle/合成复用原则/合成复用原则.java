1a0801363ffe982bd0dbb076680d0babebbb7479
dadaibiaoli <945827167@qq.com>
2023-04-02 23:19:33 +0800

    feat(GOF23):合成复用原则

---
+++ GOF23/principle/合成复用原则/合成复用原则.java
@@ -0,0 +1,10 @@
+package com.lidadaibiao.JUC.GOF23.principle.合成复用原则;
+
+/**
+ * @Author : dadaibiaoli
+ * @DateTime: 2023/4/2 23:15
+ * @Description :  尽量使用合成/聚合的方式，而不是使用继承
+ */
+public class 合成复用原则 {
+
+}
